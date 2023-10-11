d6028e32e83d259e657351fbde6429381e523bae
pedrocrohare <pedro.crohare@mercadolibre.com>
2023-10-11 18:59:06 -0300

    created project

--- CustomizableUIComponents/ContentView.swift
+++ CustomizableUIComponents/ContentView.swift
@@ -6,16 +6,45 @@
 //
 
 import SwiftUI
+import Lottie
+
+
+struct LottieView: UIViewRepresentable {
+    
+    var name: String
+    var loopMode: LottieLoopMode = .loop
+    
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        let animationView = LottieAnimationView(name: self.name)
+        animationView.loopMode = self.loopMode
+        animationView.play()
+        animationView.contentMode = .scaleAspectFit
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<LottieView>) {
+    }
+}
 
 public struct CustomContentView: View {
     public var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, custom world!")
+        HStack {
+            Spacer()
+            Text("Hello, custom world!").padding(.all)
+            Divider()
+            LottieView(name: "bouncing", loopMode: .autoReverse)
         }
-        .padding()
     }
 }
 
